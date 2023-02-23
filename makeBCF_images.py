bca3851c93b60db572f1d284254c93b05e8d15d3
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2023-02-23 14:46:37 +0000

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   makeBCF_images.py

---
+++ makeBCF_images.py
@@ -0,0 +1,13 @@
+
+#%% import packages 
+import sys
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy as scipy
+
+from readBCF import Bruker, makePics
+
+
+#%%  
+
